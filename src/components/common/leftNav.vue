28d9ee3c20b4ba2c95cbbd4c98111be476cde80f
fengfox <fengfox044@163.com>
2017-12-10 16:27:13 +0800

    changes log

---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="left-nav">
+		<ul>
+			<li>
+				<i class="icon iconfont icon-gongnengjianyi">
+				<div>收银</div>
+				</i>
+			</li>
+			<li>
+				<i class="icon iconfont icon-wo">
+				<div>我</div>
+				</i>
+			</li>
+			<li>
+				<i class="icon iconfont icon-qiehuanqizuo">
+				<div>左切换</div>
+				</i>
+			</li>
+			<li>
+				<i class="icon iconfont icon-zhubaoshipin">
+				<div>vip</div>
+				</i>
+			</li>
+			<li>
+				<i class="icon iconfont icon-liebiao">
+				<div>更多</div>
+				</i>
+			</li>
+		</ul>
+	</div>
+</template>
+<script>
+export default{
+}
+</script>
+<style>
+.left-nav{
+	color:#fff;
+	font-size:10px;
+	height:100%;
+	background-color:#1D8CE0;
+	float:left;
+	width:5%;
+}
+.iconfont{
+	font-size:24px;
+	
+}
+.left-nav ul{
+	padding:0px;
+	margin:0px;
+	
+}
+.left-nav li{
+	list.style:none;
+	text-align:center;
+	border-bottom:1px solid #20a0ff;
+	padding:10px;
+	
+}
+</style>
---
+++ src/components/page/Pos.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="pos">
+	<el-row>
+		<el-col :span="7" class="pos-order" id="order-list">
+			我是订单
+		</el-col>
+		<el-col>
+		    我是产品
+		</el-col>
+	</el-row>
+  </div>
+</template>
+
+<script  type="text/javascript">
+export default {
+  name: 'pos',
+  mounted:function()
+  {
+	document.getElementById("order-list").style.height=document.body.clientHeight+"px";
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+.pos-order{
+	background-color:#F9FAFC;
+	border-right:1solid #C0CCDA;
+}
+</style>
